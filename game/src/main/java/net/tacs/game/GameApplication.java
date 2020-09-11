7f0cd1d971e33e7a94738e2e6fcde19d61e69706
Alejandro Laconte <lacontealejandro@gmail.com>
2020-09-11 19:26:57 -0300

    Guardado en memoria en Main class

--- game/src/main/java/net/tacs/game/GameApplication.java
+++ game/src/main/java/net/tacs/game/GameApplication.java
@@ -1,21 +1,42 @@
 package net.tacs.game;
 
+import net.tacs.game.model.Match;
+import net.tacs.game.model.Municipality;
+import net.tacs.game.model.Province;
+import net.tacs.game.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class GameApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "¡Bienvenidos a TACticaS, el juego de la pandemia!";
-	}
+	private static List<Match> matches;
+	private static List<Province> provinces;
+	private static List<Municipality> municipalities;
+	private static List<User> users;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameApplication.class, args);
 	}
+
+	public static void addMatch(Match newMatch) {
+		matches.add(newMatch);
+	}
+
+	public static void addProvince(Province province) {
+		provinces.add(province);
+	}
+
+	public static void addMunicipality(Municipality newMunicipality) {
+		municipalities.add(newMunicipality);
+	}
+
+	public static void addUser(User newUser) {
+		users.add(newUser);
+	}
+
+	//Agregar los métodos para buscar/guardar en memoria que hagan falta (hasta que tengamos persistencia)
 
 }
