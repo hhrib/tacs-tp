6f52f81759072e22daa1e1c89b931bfe665a7248
Fernando Merazzi <f3rna@live.com.ar>
2020-09-16 20:53:32 -0300

    Get de users

---
+++ game/src/main/java/net/tacs/game/model/bean/UserBeanResponse.java
@@ -0,0 +1,28 @@
+package net.tacs.game.model.bean;
+
+public class UserBeanResponse {
+
+    Long id;
+
+    String name;
+
+    public UserBeanResponse() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
